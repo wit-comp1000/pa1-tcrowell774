98258e05e2b9353e56e115870a8cdc16d2311f88
crowellt1 <crowellt1@BY2133M59205826.wit.private>
2022-09-25 14:17:37 -0400

    Finished Functional program missing comments.

--- comp1000_pa1/src/comp1000_pa1/PA1a.java
+++ comp1000_pa1/src/comp1000_pa1/PA1a.java
@@ -1,5 +1,20 @@
 package comp1000_pa1;
 
+import java.util.Scanner;
+
 public class PA1a {
+	public static void main(String[] args) {
+        int Yard = 36*1;
+        int Feet = 12*1;
+        int inches = 1*1;
+        Scanner Scan1 = new Scanner(System.in);
+        System.out.println("Enter Yards");
+        int x = Scan1.nextInt();
+        System.out.println("Enter Feet");
+        int y = Scan1.nextInt();
+        System.out.println("Enter Inches");
+        int z = Scan1.nextInt();
+        System.out.println(x*36+y*12+z+" Inches");
+    }
 
 }
