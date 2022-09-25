89f29f6d0d697ee96cfe2db454fc99c6dd45400e
crowellt1 <crowellt1@BY2133M59205826.wit.private>
2022-09-25 15:03:01 -0400

    Finished project B

--- comp1000_pa1/src/comp1000_pa1/PA1b.java
+++ comp1000_pa1/src/comp1000_pa1/PA1b.java
@@ -1,8 +1,19 @@
 package comp1000_pa1;
-
+// Import Scanner code
+import java.util.Scanner;
 public class PA1b {
 	public static void main(String[] args) {
-		// your code here
+		Scanner Scan1 = new Scanner(System.in);
+		// print message for inches impute
+		System.out.println("Enter Inches");
+		// this divides the x impute by 36 to get a and takes the remainder numbers and repeats until you are left with just inches.
+		int x = Scan1.nextInt();
+		int a = x/36;
+		int y = x % 36;
+		int b = y/12;
+		int z = y % 12;
+		// print out yards feet and inches
+		System.out.println(a+" Yards "+b+" feet "+z+" inches");
 	}
 
 }
